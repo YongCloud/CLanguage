6ec42fd061dd48e272e845f937c9bb869c4c7df6
yang.yong <2315979845@qq.com>
2016-11-27 21:32:19 +0800

    the value of assignment expression

---
+++ expression.c
@@ -0,0 +1,13 @@
+/*
+	created by yangyong,Nov 27,2016
+	function:assignment expression's value
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+	int a = 0;
+	// what result will be printed?
+	printf("%d\n",a=8);
+
+	return 0;
+}
