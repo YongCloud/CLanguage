204696b4e7d4453f5acbf9f5d47df6f3eb644151
yang.yong <2315979845@qq.com>
2016-11-27 16:34:39 +0800

    print the value of constant NULL

---
+++ NULL.c
@@ -0,0 +1,10 @@
+/*
+	created by yangyong,Nov 27,2016
+	function:print the value of constant NULL.
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+	printf("NULL is :%d\n",NULL);
+	return 0;
+}
